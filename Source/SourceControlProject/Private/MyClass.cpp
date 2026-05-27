369a80fcf4fe92cbb693fab75435b325f0186582
Terrence Chan <roxes2112@yahoo.com>
2026-05-27 11:26:04 +0800

    Fix build issue
    Remove VisualStudioTools from uproject
    Add empty class and actor
    Setup default project

---
+++ Source/SourceControlProject/Private/MyClass.cpp
@@ -0,0 +1,11 @@
+// This project is made by EpicCrisis
+
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ Source/SourceControlProject/Public/MyClass.h
@@ -0,0 +1,11 @@
+// This project is made by EpicCrisis
+
+#pragma once
+#include "CoreMinimal.h"
+
+class SOURCECONTROLPROJECT_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
