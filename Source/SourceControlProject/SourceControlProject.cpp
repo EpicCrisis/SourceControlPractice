bb755133e7aff422652f7f1c938d2091406a4d5c
Terrence Chan <roxes2112@yahoo.com>
2026-05-27 09:44:35 +0800

    Initial commit

---
+++ Source/SourceControlProject/SourceControlProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SourceControlProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SourceControlProject, "SourceControlProject" );
